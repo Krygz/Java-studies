ca7a1e4d03e2841db583269783e8bca6007d7180
Pedro Henrique <139001941+Krygz@users.noreply.github.com>
2023-09-20 18:09:17 -0300

    array.java

---
+++ array.java
@@ -0,0 +1,31 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		String[] cars = new String[4];
+		
+	cars[0] = "Camaro";
+	cars[1] = "corvette";
+	cars[2] = "mustang";
+	cars[3] = "fusca";
+	
+	for(int i = 0; i<cars.length;i++) {
+		System.out.println(cars[i]);
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+   }
+   
+		
+		
+		
+	}
