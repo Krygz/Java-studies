2be84d0e9fcd0c12747a7a712b39db4d395d47a2
Pedro Henrique <139001941+Krygz@users.noreply.github.com>
2023-09-20 19:04:40 -0300

    2d-arrays.java

---
+++ 2d-arrays.java
@@ -0,0 +1,52 @@
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		String[][] cars = new String[3][3];
+		
+	cars[0][0] = "Camaro";
+	cars[0][1] = "corvette";
+	cars[0][2] = "mustang";
+
+	
+	cars[1][0] = "fusca";
+	cars[1][1] = "uno";
+	cars[1][2] = "ferrari";
+
+	cars[2][0] = "lancer";
+	cars[2][1] = "suv";
+	cars[2][2] = "supra";
+	
+	
+	
+	for(int i = 0; i<cars.length;i++) {
+		System.out.println();
+		for(int j = 0 ;j<cars[i].length;j++) {
+			System.out.print(cars[i][j]+" ");
+			
+			
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+   }
+   
+		
+		
+		
+	}
+
+
