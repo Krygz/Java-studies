18b9cbe8f0608084574c031c36f3f4778cf0e378
Pedro Henrique <139001941+Krygz@users.noreply.github.com>
2023-09-24 18:09:23 -0300

    printf.java

---
+++ printf.java
@@ -0,0 +1,53 @@
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	boolean myboolean = true;
+	char mychar = '@';
+	String mystring = "hi";
+	int myint = 50;
+	double mydouble = 1000;
+		
+		//CONVERSIONS
+	//System.out.printf("%b" , myboolean );
+	//System.out.printf("%c" , mychar );
+	//System.out.printf("%s" , mystring );	
+	//System.out.printf("%d" , myint );	
+	//System.out.printf("%f" , mydouble);	
+		
+		
+		//WIDTH
+	//System.out.printf("hello %10s " ,mystring);
+		
+		
+	//PRECISION
+	//System.out.printf("you have this much money %.2f" , mydouble);
+	
+	//FLAGS
+	//System.out.printf("you have this much money %,f" , mydouble);
+	}
+	
+	}
+
+		
+		
+	
+	
+	
+	
+	
+	
+		
+		
+		
+	
+		
+ 
+		
+		
+		
+
+
+
