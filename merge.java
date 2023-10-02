93535c5bdc2173a2d4023803b7ff839dc495258b
Pedro Henrique <139001941+Krygz@users.noreply.github.com>
2023-10-02 19:59:39 -0300

    Update merge.java

--- merge.java
+++ merge.java
@@ -6,7 +6,7 @@ public class Main {
 	
 	public static void main(String[] args){
 		
-  int [ ] arr1 = {1,2,3,4,5};
+  int [] arr1 = {1,2,3,4,5};
   int [] arr2 = {6,7,8,9};
       
   int n1 = arr1.length;
